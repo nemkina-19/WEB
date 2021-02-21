36d6ad64fb88d24c3f8a0a180f9b5ebdf809e753
Солнышко <nemina.pe@yandex.ru>
2021-02-21 18:25:22 +0500

    реклама с картинкой

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='/css/style.css')}" />
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/static/img/MARS.png">
+                    <div class="alert alert-primary" role="alert">
+                      Человечество вырастает из дерева.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемым безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                    И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                    Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
